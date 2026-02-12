86ebfb8151d82f2fad09b2b5f44c4987e39b6e5d
Pankaj-Neosao <pankaj.yewale@neosao.co.in>
2026-02-12 13:08:07 +0530

    updated at 12-feb-26

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from PIL import Image
 import logging
 import json
 import io
+import os
 from flask import Flask, request, jsonify
 from flask import render_template
 
@@ -185,4 +186,5 @@ def predict():
 # Run Server
 # =======================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
